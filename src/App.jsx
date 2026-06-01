a3f5b3c72aa12ba344a06aef7ada264362bddb21
Wisey <okun5315455@gmail.com>
2026-06-01 14:38:42 +0200

    feat: add Homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './navbar'
-
+import Home from './Home'
 function App(){
   return(
     <>
       <NavBar></NavBar>
+      <Home></Home>
     </>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+
+
+function Home(){
+    return(
+    <>
+       <div className="homediv">
+          <h1 className="homeheader">welcom to our shop..</h1>
+          <p className="homepara">Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. At eius rerum dolorem
+            qui quidem quibusdam!
+            Nobis nostrum accusamus, in, quod ad neque
+            natus, delectus ut quisquam facilis rem. Eum,
+            aspernatur!</p>
+            <button className="homebtn">Shop now!</button>
+       </div>
+    </>
+    )
+}
+
+export default Home
