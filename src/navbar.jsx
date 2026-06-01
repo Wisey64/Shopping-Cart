56d8a77c85ec4b3d52bed4699d774bb44a2f8bf9
Wisey <okun5315455@gmail.com>
2026-06-01 14:18:00 +0200

    feat: add basic  navbar component

---
+++ src/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function NavBar(){
+    return(
+    <>
+        <div className="navbar">
+            <div className="leftnav">
+                <img src="" alt="logo" className="storelogo" />
+                <h1 className="mystore">mystore</h1>
+            </div>
+            <div className="rightnav">
+                <button>Home</button>
+                <button>Shop</button>
+                <button>Cart</button>
+            </div>
+        </div>
+    </>
+    ) 
+}
